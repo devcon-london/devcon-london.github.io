2fbc1e6f80a7e30d32ac87c12bea4c03ae97d8b9
Pedro Duarte <contact@peduarte.com>
2015-07-25 19:19:29 +0100

    Adding some styles

--- client/src/app.js
+++ client/src/app.js
@@ -7,6 +7,6 @@ import Form from './components/form/form.js';
 var devConFormId = '1FvacYUr7XxypGawVUOquLRdoH_QNy1wRxiCMZ6SN304';
 var testFormId = '1gNrrrv8vzIk2tJFDB0Nd3XZzCODCZNn1Q7MxXKQ1hes';
 React.render(
-  <Form id={testFormId} />,
+  <Form id={devConFormId} />,
   document.getElementById('app')
 );
