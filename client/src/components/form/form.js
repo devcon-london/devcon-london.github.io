a25c14a256db0472cee1f1781f8b6e67ad7938f0
Pedro Duarte <contact@peduarte.com>
2015-07-27 21:05:06 +0100

    Removing jquery and adding browser-request

--- client/src/components/form/form.js
+++ client/src/components/form/form.js
@@ -3,21 +3,20 @@ import './form.css';
 import React from 'react';
 import FieldList from '../field-list/field-list.js';
 
+import request from 'browser-request';
+
 var endpoint = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api/google-form?formId=' : 'https://devconlondon.herokuapp.com/api/google-form?formId=';
 
 export default React.createClass({
   loadDataFromServer() {
-    $.ajax({
-      url: endpoint + this.props.id,
-      dataType: 'json',
-      success: function(data) {
-        $('#loading').hide();
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(status, err.toString());
-      }.bind(this)
-    });
+    var self = this;
+
+    request(endpoint + this.props.id, function(error, response, body) {
+      if (error) { throw error; }
+
+      document.getElementById('loading').style.display = 'none';
+      self.setState({data: JSON.parse(body)});
+    })
   },
 
   getInitialState() {
