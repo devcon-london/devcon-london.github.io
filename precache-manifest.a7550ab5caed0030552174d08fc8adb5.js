837139a5a96fc32a09d6898790117c11e9671902
Thomas Alisi <thomasalisi@gmail.com>
2019-05-12 11:08:04 +0100

    run the app, live

---
+++ precache-manifest.a7550ab5caed0030552174d08fc8adb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4448c05dab9500d268ec4c106ebdc01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6ceff76770d719c2d5f",
+    "url": "/static/css/main.3a3472d5.chunk.css"
+  },
+  {
+    "revision": "4db0d8d278509bddbf09",
+    "url": "/static/js/2.ee25a5d6.chunk.js"
+  },
+  {
+    "revision": "b6ceff76770d719c2d5f",
+    "url": "/static/js/main.6b3c5f8d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
